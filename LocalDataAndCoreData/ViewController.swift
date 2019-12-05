6e3282913503a67d0e54ff0f8b4959552b5afd63
Reynard Vincent Nata <reynardvnata@yahoo.co.id>
2019-12-05 10:27:49 +0700

    Initial Commit

---
+++ LocalDataAndCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocalDataAndCoreData
+//
+//  Created by Reynard Vincent Nata on 05/12/19.
+//  Copyright © 2019 Reynard Vincent Nata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LocalDataAndCoreDataTests/LocalDataAndCoreDataTests.swift
@@ -0,0 +1,34 @@
+//
+//  LocalDataAndCoreDataTests.swift
+//  LocalDataAndCoreDataTests
+//
+//  Created by Reynard Vincent Nata on 05/12/19.
+//  Copyright © 2019 Reynard Vincent Nata. All rights reserved.
+//
+
+import XCTest
+@testable import LocalDataAndCoreData
+
+class LocalDataAndCoreDataTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
